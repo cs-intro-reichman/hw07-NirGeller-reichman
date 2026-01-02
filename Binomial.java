f0aa271023ec72b8e1f799368ac5e57e555fe8c0
NirGeller-reichman <nir.gellerchipchev@post.runi.ac.il>
2026-01-02 21:12:57 +0200

    second binomial

--- Binomial.java
+++ Binomial.java
@@ -21,9 +21,9 @@ return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
 	// Computes the Binomial function, efficiently
 	public static int binomial(int n, int k) {
 		int[][] memo= new int [n+1][k+1];
-		for(int i=0; i<n;i++ ){
-			for(int j=0;j<k;j++){
-				memo[i][j]=1;
+		for(int i=0; i<=n;i++ ){
+		for(int j=0;j<=k;j++){
+				memo[i][j]=-1;
 			}
 			
 		}
